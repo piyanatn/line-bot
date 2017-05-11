a3b24b2f9c6396e8e52256b890e39ecc82695f91
Piyanat Nimkhuntod <piyanat.nt@Piyanats-MacBook-Pro.local>
2017-05-11 23:04:39 +0700

    Modify bot.php

--- bot.php
+++ bot.php
@@ -1,22 +1,6 @@
 <?php
 
-//READ DRUG ALLEGRY
-$line_url = 'http://110.77.146.4:3000/api/drugallergy/3670500981816';
-$chl = curl_init($line_url);
-curl_setopt($chl, CURLOPT_TIMEOUT, 5);
-curl_setopt($chl, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($chl, CURLOPT_RETURNTRANSFER, true);
-$data1 = curl_exec($chl);
-curl_close($chl);
-//echo $data;
-$json = file_get_contents($line_url);
-$obj = json_decode($json,true);
 
-//. "<br>" .
-//$obj['opd_allergy'][0]['symptom'];
-
-// Build message to reply back
-$mymessage = 'คำเตือน : '. $obj['opd_allergy'][0]['agent'].'<br>'.$obj['opd_allergy'][0]['symptom'];
 
 $access_token = 'tMQUkt2NlMGyROq8or9Yo//dLL20vznurHf/wundj5T+PeDLqoXRvZLY+5Drmoz0NXmRQy97b/xssaSunifqxDGQnu7faRK6rMDQEwEx0yiox8RJdzGJxcKblQA3Qb7DBq85P4m7pobxDTRbJ1WKIQdB04t89/1O/w1cDnyilFU=';
 
@@ -34,6 +18,25 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['message']['text'];
 			//$text = 'คำเตือน : '.$obj['opd_allergy'][0]['agent'];
+
+			//READ DRUG ALLEGRY
+			$line_url = 'http://110.77.146.4:3000/api/drugallergy/'.$text;
+			$chl = curl_init($line_url);
+			curl_setopt($chl, CURLOPT_TIMEOUT, 5);
+			curl_setopt($chl, CURLOPT_CONNECTTIMEOUT, 5);
+			curl_setopt($chl, CURLOPT_RETURNTRANSFER, true);
+			$data1 = curl_exec($chl);
+			curl_close($chl);
+			//echo $data;
+			$json = file_get_contents($line_url);
+			$obj = json_decode($json,true);
+
+			//. "<br>" .
+			//$obj['opd_allergy'][0]['symptom'];
+
+			// Build message to reply back
+			$mymessage = 'คำเตือน : '. $obj['opd_allergy'][0]['agent'].'<br>'.$obj['opd_allergy'][0]['symptom'];
+
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
